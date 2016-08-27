5f29ae3e5a8af14f751364a2261b075156f5643e
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-08-27 22:53:48 +0530

    Basic Application and HTML Aware Widgets

---
+++ minimal/main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[]){
+    QApplication app(argc,argv);
+    QLabel *label = new QLabel("<h2>Hello</h2> <font color=red><i>world</i></font>");
+
+    label->show();
+    return app.exec();
+}
