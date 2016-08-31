a1e29482f1884804398647fe78f095f5ee7e8f9e
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-09-01 02:12:06 +0530

    C++ Qt 17 - QCheckBox: Created GUI app 17-QCheckBox

---
+++ 17-QCheckBox/17-QCheckBox.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-09-01T02:06:11
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = 17-QCheckBox
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        dialog.cpp
+
+HEADERS  += dialog.h
+
+FORMS    += dialog.ui
---
+++ 17-QCheckBox/dialog.cpp
@@ -0,0 +1,25 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+#include <QMessageBox>
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_pushButton_clicked()
+{
+    if(ui->checkBox->isChecked()){
+        QMessageBox::information(this,"Title","You like cats!!");
+    }
+    else{
+        QMessageBox::information(this,"Title","You don't like cats!!");
+    }
+}
