099b00dcf4412e61559f76588f92343d8af5ef8d
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-09-03 16:58:37 +0530

    C++ Qt 21 - QTreeWidget: Created GUI app 21-QTreeWidget

---
+++ 21-QTreeWidget/21-QTreeWidget.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-09-03T16:25:11
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = 21-QTreeWidget
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        dialog.cpp
+
+HEADERS  += dialog.h
+
+FORMS    += dialog.ui
---
+++ 21-QTreeWidget/dialog.h
@@ -0,0 +1,25 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QTreeWidgetItem>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+    void AddRoot(QString name,QString Description);
+    void AddChild(QTreeWidgetItem *parent, QString name,QString Description);
+
+public:
+    explicit Dialog(QWidget *parent = 0);
+    ~Dialog();
+
+private:
+    Ui::Dialog *ui;
+};
+
+#endif // DIALOG_H
