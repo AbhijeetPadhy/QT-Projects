314568347441518d562b4f9a081af798cda163f7
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-09-06 12:03:16 +0530

    C++ Qt 27 - QTimer: Implemented QTimer

--- 27-QTimer/mytimer.cpp
+++ 27-QTimer/mytimer.cpp
@@ -1,6 +1,13 @@
 #include "mytimer.h"
+#include <QDebug>
 
 MyTimer::MyTimer()
 {
+    timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(MySlot()));
+            timer->start(1000);
+}
 
+void MyTimer::MySlot(){
+    qDebug() << "Timer Executed";
 }
