912c6543e42c5ee7a331f37f6b0f6092c0af2ccd
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-09-09 22:01:04 +0530

    C++ Qt 28 - QThread part 1 creating a thread: Finalised the console app 28-QThread1

--- 28-QThread1/mythread.cpp
+++ 28-QThread1/mythread.cpp
@@ -1,6 +1,15 @@
 #include "mythread.h"
+#include <QDebug>
 
 MyThread::MyThread()
 {
 
+}
+
+void MyThread::run(){
+    qDebug() << this->name << "Running";
+
+    for(int i=0;i<100;i++){
+        qDebug() << this->name << i;
+    }
 }
--- 28-QThread1/mythread.h
+++ 28-QThread1/mythread.h
@@ -1,11 +1,13 @@
 #ifndef MYTHREAD_H
 #define MYTHREAD_H
+#include <QThread>
 
-
-class MyThread
+class MyThread: public QThread
 {
 public:
     MyThread();
+    void run();//overriding run
+    QString name;
 };
 
 #endif // MYTHREAD_H
