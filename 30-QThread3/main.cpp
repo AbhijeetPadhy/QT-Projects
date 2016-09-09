8be41309f9f14f378831cac0556424e81b14d216
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-09-09 23:17:02 +0530

    C++ Qt 30 - QThread part 3 the QMutex: Finalised console app 30-QThread3

--- 30-QThread3/main.cpp
+++ 30-QThread3/main.cpp
@@ -1,8 +1,23 @@
 #include <QCoreApplication>
-
+#include <mythread.h>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    MyThread mThread1;
+    mThread1.name = "MyThread1";
+
+    MyThread mThread2;
+    mThread2.name = "MyThread2";
+
+    MyThread mThread3;
+    mThread3.name = "MyThread3";
+
+    mThread1.start();
+    //mThread2.start();
+    //mThread3.start();
+
+    mThread1.Stop = true;
 
     return a.exec();
 }
--- 30-QThread3/mythread.h
+++ 30-QThread3/mythread.h
@@ -1,11 +1,14 @@
 #ifndef MYTHREAD_H
 #define MYTHREAD_H
+#include <QThread>
 
-
-class MyThread
+class MyThread: public QThread
 {
 public:
     MyThread();
+    QString name;
+    void run();
+    bool Stop;
 };
 
 #endif // MYTHREAD_H
