d8f917c4baddb702e3c956690bb1b09d969b0cab
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2016-09-09 22:18:27 +0530

    C++ Qt 29 - QThread part 2 the Priority: Finalised Console app 29-QThread2

--- 29-QThread2/mythread.cpp
+++ 29-QThread2/mythread.cpp
@@ -1,6 +1,13 @@
 #include "mythread.h"
-
+#include <QDebug>
 MyThread::MyThread()
 {
 
+}
+
+void MyThread::run(){
+    qDebug() << this->name << " Running";
+
+    for(int i=0;i<10000;i++)
+        qDebug() << this->name << i;
 }
